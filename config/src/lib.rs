beb79f1607635eddc12aaab4a36b28afd7de0e26
Kay <kehiiiiya@gmail.com>
2023-10-31 01:53:00 +0800

    test

--- config/src/lib.rs
+++ config/src/lib.rs
@@ -72,10 +72,44 @@ impl Config {
 
 #[cfg(test)]
 mod tests {
+    use std::io::Write;
+
     use super::*;
 
+    fn temp_config_file() {
+        let mut p = std::env::temp_dir();
+        p.push("./config.toml");
+        let mut file = match std::fs::File::create(p) {
+            Ok(file) => file,
+            Err(e) => {
+                eprintln!("Failed to create temp config file: {}", e);
+                return;
+            }
+        };
+        if let Err(e) = file.write_all(
+            b"
+[network]
+port = 37771
+max_connections = 10
+[nostr]
+port = 4444
+max_ws_connection = 1000000
+[rpc]
+enable_rpc = true
+[metrics]
+enable_metrics = false
+[log]
+write_to_file = true
+path = 'log.r7'
+        ",
+        ) {
+            eprintln!("Failed to write to temp config file: {}", e);
+        }
+    }
+
     #[test]
-    fn config() {
+    fn load_from_file_test() {
+        temp_config_file();
         let loaded_from_file = Config::load_from_file("./config.toml").unwrap();
         let default = Config::default().unwrap();
 
@@ -103,5 +137,23 @@ mod tests {
         assert_eq!(loaded_from_file.nostr.port, default.nostr.port);
 
         assert_eq!(loaded_from_file.rpc.enable_rpc, default.rpc.enable_rpc);
+    }
+
+    #[test]
+    fn default_test() {
+        let default = Config::default().unwrap();
+
+        assert_eq!("log.r7", default.log.path);
+        assert_eq!(true, default.log.write_to_file);
+
+        assert_eq!(false, default.metrics.enable_metrics);
+
+        assert_eq!(10, default.network.max_connections);
+        assert_eq!(37771, default.network.port);
+
+        assert_eq!(1000000, default.nostr.max_ws_connections);
+        assert_eq!(4444, default.nostr.port);
+
+        assert_eq!(true, default.rpc.enable_rpc);
     }
 }
