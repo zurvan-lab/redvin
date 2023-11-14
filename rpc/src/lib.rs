53753b6b20ba832b370ff30c0935405a377e4b16
Kayhan Alizadeh <kehiiiiya@gmail.com>
2023-11-15 05:02:49 +0800

    feat(gRPC): grpc proto files done, grpc server initiated, config updated (#41)

--- rpc/src/lib.rs
+++ rpc/src/lib.rs
@@ -1,1 +1,9 @@
+use std::io;
+// use tonic::{transport::Server, Request, Response, Status};
 
+#[tokio::main]
+pub async fn start(_port: u16) -> Result<(), io::Error> {
+    // let addr = format!("[::1]:{}", port).parse().unwrap();
+    // Server::builder().add_service().serve(addr).await;
+    Ok(())
+}
