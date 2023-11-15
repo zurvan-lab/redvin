c91721008693042328958eed771ab6a7ba95e4eb
Kayhan Alizadeh <kehiiiiya@gmail.com>
2023-11-16 06:03:32 +0800

    feat: gRPC library initiated (#44)

--- rpc/src/lib.rs
+++ rpc/src/lib.rs
@@ -1,9 +1,3 @@
-use std::io;
-// use tonic::{transport::Server, Request, Response, Status};
-
-#[tokio::main]
-pub async fn start(_port: u16) -> Result<(), io::Error> {
-    // let addr = format!("[::1]:{}", port).parse().unwrap();
-    // Server::builder().add_service().serve(addr).await;
-    Ok(())
-}
+pub mod network;
+pub mod nostr;
+pub mod server;
---
+++ rpc/src/network.rs
@@ -0,0 +1,56 @@
+use tonic::{Request, Response, Status};
+
+use network::network_server::Network;
+use network::{
+    GetNetworkInfoRequest, GetNetworkInfoResponse, GetNodeInfoRequest, GetNodeInfoResponse,
+    PeerInfo,
+};
+
+pub mod network {
+    tonic::include_proto!("network");
+}
+
+#[derive(Debug, Default)]
+pub struct NetworkService {}
+
+#[tonic::async_trait]
+impl Network for NetworkService {
+    async fn get_network_info(
+        &self,
+        request: Request<GetNetworkInfoRequest>,
+    ) -> Result<Response<GetNetworkInfoResponse>, Status> {
+        let _req = request.into_inner();
+
+        let reply = GetNetworkInfoResponse {
+            total_sent_bytes: 0,
+            total_received_bytes: 0,
+            started_at: 0,
+            peers: vec![PeerInfo {
+                address: "".to_string(),
+                agent: "".to_string(),
+                moniker: "".to_string(),
+                last_received: 0,
+                last_sent: 0,
+                status: 0,
+                peer_id: vec![0],
+            }],
+        };
+
+        Ok(Response::new(reply))
+    }
+
+    async fn get_node_info(
+        &self,
+        request: Request<GetNodeInfoRequest>,
+    ) -> Result<Response<GetNodeInfoResponse>, Status> {
+        let _req = request.into_inner();
+
+        let reply = GetNodeInfoResponse {
+            moniker: "".to_string(),
+            agent: "".to_string(),
+            peer_id: vec![0],
+        };
+
+        Ok(Response::new(reply))
+    }
+}
