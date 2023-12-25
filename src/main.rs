571008ace2e83788592070dd8ad3d733d7349b24
Kay <kehiiiiya@gmail.com>
2023-12-25 22:23:52 +0000

    feat: initializing cli for node initing and running

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::*;
+
 fn main() {
-    println!("Hello, world!");
+    let init_command = command!()
+    .name("init")
+    .about("Initializing a Redvin node.")
+    .arg(
+        Arg::new("working-directory")
+        .long("working-directory")
+        .short('w')
+        .help("Working directory is the path for saving relay, peers data and some other stuff for managing node.")
+        .aliases(["workdir", "wdir", "workingdirectory", "workingdir", "wdirectory"])
+        .required(true)
+    );
+
+    let start_command = command!()
+        .name("start")
+        .about("Starting a Redvin instance by passing a working directory.");
+
+    let _root_command = command!()
+        .about("Redvin is an IPNN implementation in rust, helping to build decentralized future.")
+        .subcommand(init_command)
+        .subcommand(start_command)
+        .get_matches();
 }
