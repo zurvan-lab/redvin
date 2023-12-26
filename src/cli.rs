48955f4f801a66a42db47dc348212b11e310fc69
Kay <kehiiiiya@gmail.com>
2023-12-26 16:08:49 +0000

    chore: enhancing redvin cli tool

---
+++ src/cli.rs
@@ -0,0 +1,25 @@
+use clap::{command, Arg, ArgMatches};
+
+pub fn command() -> ArgMatches {
+    let init_command = command!()
+    .name("init")
+    .about("Initializing a Redvin node.")
+    .arg(
+        Arg::new("working-directory")
+        .long("working-directory")
+        .short('w')
+        .help("Working directory is the path for saving relay, peers data and some other stuff for managing node.")
+        .aliases(["workdir", "wdir", "workingdirectory", "workingdir", "wdirectory"])
+        .required(true)
+    );
+
+    let start_command = command!()
+        .name("start")
+        .about("Starting a Redvin instance by passing a working directory.");
+
+    command!()
+        .about("Redvin is an IPNN implementation in rust, helping to build decentralized future.")
+        .subcommand(init_command)
+        .subcommand(start_command)
+        .get_matches()
+}
