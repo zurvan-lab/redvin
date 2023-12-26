d344be5aeccb347e4bb787d5d305c1f2b76d7816
Kay <kehiiiiya@gmail.com>
2023-12-26 15:55:11 +0000

    chore: writing start command

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use clap::*;
+use config::Config;
+use std::fs;
 
 fn main() {
     let init_command = command!()
@@ -17,9 +19,35 @@ fn main() {
         .name("start")
         .about("Starting a Redvin instance by passing a working directory.");
 
-    let _root_command = command!()
+    let root_command = command!()
         .about("Redvin is an IPNN implementation in rust, helping to build decentralized future.")
         .subcommand(init_command)
         .subcommand(start_command)
         .get_matches();
+
+    match root_command.subcommand() {
+        Some(subcommand) => {
+            if subcommand.0 == "init" {
+                let dir_builder = fs::DirBuilder::new();
+
+                let working_directory = subcommand
+                    .1
+                    .get_one::<String>("working-directory")
+                    .expect("invalid working directory path.");
+
+                dir_builder
+                    .create(working_directory)
+                    .expect("can not create working directory.");
+
+                let config_path = format!("{}/config.toml", working_directory);
+                fs::write(config_path, Config::default_file())
+                    .expect("can not create config file on working directory.");
+            } else if subcommand.0 == "start" {
+                println!("not implemented yet!")
+            }
+        }
+        None => {
+            println!("please use `redvin --help`")
+        }
+    }
 }
